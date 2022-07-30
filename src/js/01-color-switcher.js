86666a4279a6017335d6561b4d00008308a993ef
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-30 15:00:57 +0300

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnClose = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+btnClose.setAttribute('disabled', 'disabled');
 
+btnStart.addEventListener('click', changeColor);
+btnClose.addEventListener('click', stopColorChanges);
+
+
+function changeColor() {
+   timerId = setInterval(() => {
+       const newColor = getRandomHexColor();
+       bodyEl.style.backgroundColor = newColor;
+   }, 1000);
+    
+    btnStart.setAttribute('disabled', 'disabled');
+    btnClose.removeAttribute('disabled')    
+}
+
+function stopColorChanges() {    
+    btnStart.removeAttribute('disabled');
+    btnClose.setAttribute('disabled', 'disabled');
+    clearInterval(timerId);  
+} 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
