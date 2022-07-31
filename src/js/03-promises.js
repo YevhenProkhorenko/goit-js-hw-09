886821b5ed4fb14d3d48eb1c1e58d6a55b282ae6
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-31 13:53:46 +0300

    task 3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('button');
+
+const firsDelay = form.elements.delay.value;
+const delayStep = form.elements.step.value;
+const amountProm = form.elements.amount.value;
+let promtCounter = 0;
+
+let timerID = 0;
+// console.log(form.elements.delay.value);
+
+
+submitBtn.addEventListener('click', createPromise);
+
+function createPromise(position, delay) {  
+  const shouldResolve = Math.random() > 0.3;  
+  
+  setTimeout(() => {
+    if (shouldResolve) {
+      timerID = setInterval(() => {
+      position = Number(amountProm);
+      delay = Number(delayStep);
+        console.log('position:', position)
+        console.log('delay:' , delay)
+      if (promtCounter === amountProm) {
+        return;
+      }        
+      return new Promise(({ position, delay }) => {
+        
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+    }, delay);
+    }
+    else {
+      clearInterval(timerID);
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`)
   }
+  }, firsDelay)     
 }
