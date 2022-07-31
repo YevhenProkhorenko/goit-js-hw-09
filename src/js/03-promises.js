4260e73ecd489a4c8a35f5aa3b55561499635bca
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-31 21:53:46 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,39 +1,71 @@
 const form = document.querySelector('.form');
 const submitBtn = document.querySelector('button');
 
-const firsDelay = form.elements.delay.value;
-const delayStep = form.elements.step.value;
-const amountProm = form.elements.amount.value;
+
+const firsDelay = Number(form.elements.delay.value);
+const delayStep = Number(form.elements.step.value);
+const amountProm = Number(form.elements.amount.value);
+
+
 let promtCounter = 0;
 
 let timerID = 0;
-// console.log(form.elements.delay.value);
 
 
-submitBtn.addEventListener('click', createPromise);
 
-function createPromise(position, delay) {  
-  const shouldResolve = Math.random() > 0.3;  
+submitBtn.addEventListener('click', pushSubmit);
+
+function pushSubmit() {
+
+  setInterval(() => {  
+   createPromise(amountProm, delayStep)
+  }, delayStep);
   
-  setTimeout(() => {
-    if (shouldResolve) {
-      timerID = setInterval(() => {
-      position = Number(amountProm);
-      delay = Number(delayStep);
-        console.log('position:', position)
-        console.log('delay:' , delay)
-      if (promtCounter === amountProm) {
-        return;
-      }        
-      return new Promise(({ position, delay }) => {
-        
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-      })
-    }, delay);
-    }
-    else {
-      clearInterval(timerID);
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`)
-  }
-  }, firsDelay)     
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  setTimeout(() => {
+    return new Promise((resolve, reject) => {
+  if (shouldResolve) {
+    resolve(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+  } else {     
+    reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`)); 
+  }
+  })
+  }, firsDelay);
+}
+
+
+
+
+
+
+
+// ///////// НЕ правильний варіант
+// function createPromise(position, delay) {  
+//   const shouldResolve = Math.random() > 0.3;  
+  
+//   setTimeout(() => {
+//     if (shouldResolve) {
+//       timerID = setInterval(() => {
+//       position = Number(amountProm);
+//       delay = Number(delayStep);
+//         console.log('position:', position)
+//         console.log('delay:' , delay)
+//       if (promtCounter === amountProm) {
+//         return;
+//       }        
+//       return new Promise(({ position, delay }) => {
+        
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//       })
+//     }, delay);
+//     }
+//     else {
+//       clearInterval(timerID);
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+//   }
+//   }, firsDelay)     
+// }
