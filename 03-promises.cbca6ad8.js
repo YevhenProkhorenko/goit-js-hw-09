a84544d513fd5a63febb32430e218bd8a26ab6c6
YevhenProkhorenko <97792710+YevhenProkhorenko@users.noreply.github.com>
2022-08-01 17:27:28 +0000

    Deploying to gh-pages from @ YevhenProkhorenko/goit-js-hw-09@06e70590e5fa8953aa6fb349f6c92885ac24a85c 🚀

--- 03-promises.cbca6ad8.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),t=document.querySelector("button"),n=Number(e.elements.delay.value),m=(Number(e.elements.step.value),Number(e.elements.amount.value));function o(e,t){const m=Math.random()>.3;return new Promise(((o,u)=>{setTimeout((()=>{m?o(`✅ Fulfilled promise ${e} in ${t}ms`):u(`❌ Rejected promise ${e} in ${t}ms`)}),n)}))}t.addEventListener("click",(function(){for(let e=0;e<=m;e+=1)o(e,n).then().catch()}));
-//# sourceMappingURL=03-promises.cbca6ad8.js.map
